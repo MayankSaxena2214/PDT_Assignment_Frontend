c81ea569f54c30c8de3cb6fe9bdf19296ba24126
Mayank Saxena <mayanksaxena2214@gmail.com>
2024-10-23 20:49:20 +0530

    Get Started Handelled

--- src/components/Home/HeroSection/HeroSection.jsx
+++ src/components/Home/HeroSection/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./HeroSection.css";
+import { Link } from 'react-router-dom';
 
 const HeroSection = () => {
   return (
@@ -8,7 +9,7 @@ const HeroSection = () => {
             <h1>Want to earn Money?</h1>
             <h2>Make your team now and win 1 Lakh+</h2>
             <p>You can participate for just ₹10</p>
-            <button className="btn">Get Started</button>
+            <Link to={"/matches"}><button className="btn">Get Started</button></Link>
         </div>
     </div>
   );
